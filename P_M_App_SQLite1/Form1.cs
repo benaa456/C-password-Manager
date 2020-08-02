73c00f0f7be2fc3699a5593bf882d67fe718700c
benaa456 <67800857+benaa456@users.noreply.github.com>
2020-08-02 08:24:43 -0500

    Add files via upload

---
+++ P_M_App_SQLite1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//show all files
+//Right Click include in porject
+//App.config
+//Right click Refrences + Manage NuGet Packages
+//System.Data.SQLite.Core
+//Dapper
+namespace P_M_App_SQLite1
+{
+    public partial class Form1 : Form
+    {
+        List<UserInfo> listUserInfo = new List<UserInfo>();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadUserInfoList();
+            
+        }
+
+        public void LoadGridView()
+        {//inserts the data from the List into the dataGrid Table
+            
+            if(listUserInfo.Count > 0)
+            {//so the program wont crash if the dataGridView is empty
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+            }
+            foreach (var list in listUserInfo)
+            {
+                dataGridView1.Rows.Add(list.url, list.userName, list.password, list.id );
+            }
+        }
+
+        public void LoadUserInfoList()
+        {//gets data from the database, adds it to the List<UserInfo>, reloads dataGrid Table
+            listUserInfo.Clear();
+            listUserInfo = SQLiteDataAccess.LoadUser();
+            LoadGridView();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TODO add url, usename, password
+            Form_SaveToTable EnterToDatabase = new Form_SaveToTable();
+            EnterToDatabase.ShowDialog();
+            LoadUserInfoList();
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            copyToClipboard("url_column");
+            
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            copyToClipboard("userName_column");
+            
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            copyToClipboard("password_column");
+           
+        }
+
+        private void copyToClipboard(string columnName)
+        {
+            //gets the selected cell in the table and copies it to the clipboard
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowindex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = dataGridView1.Rows[rowindex];
+                string cellSelected = Convert.ToString(selectedRow.Cells[columnName].Value);
+                Clipboard.SetText(cellSelected);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//gets the selected cell id and deletes it from the database
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowindex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = dataGridView1.Rows[rowindex];
+                string cellSelected = Convert.ToString(selectedRow.Cells["id_column"].Value);
+                SQLiteDataAccess.removeItemFromSQLite(cellSelected);
+            }
+            LoadUserInfoList();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {//generate password
+         //Membership.GeneratePassword(12, 1);
+            textBox1.Clear();
+            string psw = CreateRandomPassword();
+            textBox1.Text = psw;
+
+        }
+        private static string CreateRandomPassword(int length = 15)
+        {
+            // Create a string of characters, numbers, special characters that allowed in the password  
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            // Select one random character at a time from the string  
+            // and create an array of chars  
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+    }
+}
---
+++ P_M_App_SQLite1/Form_SaveToTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_M_App_SQLite1
+{
+    // TODO center this Form_SaveTOTable inside Form1
+    public partial class Form_SaveToTable : Form
+    {
+        public Form_SaveToTable()
+        {
+            InitializeComponent();
+            textBox3.PasswordChar = '*';
+            textBox4.PasswordChar = '*';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//close button
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//add button
+            string url, username, password, password2;
+            url = textBox1.Text;
+            username = textBox2.Text;
+            password = textBox3.Text;
+            password2 = textBox4.Text;
+            if (password.Equals(password2))
+            {
+                addtosql(url, username, password);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                // TODO Reload DataGridView from Form1 window w/o closing this form
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("The Password dosn't match");
+            }
+            
+        }
+        public void addtosql(string url, string username, string password)
+        {//stores data into the database
+            UserInfo user = new UserInfo();
+            user.url = url;
+            user.userName = username;
+            user.password = password;
+            SQLiteDataAccess.SaveUser(user);
+        }
+
+    }
+}
